0f4b4d9bdcbdcef5ad242c10c50fabbd96de125b
canerkaseler <caner@sidekickinteractive.com>
2023-06-23 23:32:59 -0400

    Created basic Lottie animation on compose screen.

---
+++ app/src/main/java/com/canerkaseler/composelottie/ComposeLottieScreen.kt
@@ -0,0 +1,43 @@
+package com.canerkaseler.composelottie
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun ComposeLottieScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White)
+    ){
+        ComposeLottieAnimation(modifier = Modifier.align(alignment = Alignment.Center))
+    }
+}
+
+@Composable
+fun ComposeLottieAnimation(modifier: Modifier) {
+
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.anim_loading_success_failed))
+
+    LottieAnimation(
+        modifier = modifier,
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+    )
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    ComposeLottieScreen()
+}
