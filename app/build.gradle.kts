990fff9bad05b18a1a03992fcc333d8a07254315
canerkaseler <caner@sidekickinteractive.com>
2023-06-23 23:31:22 -0400

    Added Lottie library into project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 
 dependencies {
 
+    implementation("com.airbnb.android:lottie-compose:6.0.1")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
